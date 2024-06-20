2257614e17bebca3abde99174dbcc4954aae4cf2
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-06-20 16:44:44 +0500

    files updated

--- app/src/main/java/com/example/wishlistapp/AppBar.kt
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -1,34 +1,65 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.example.wishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBarView(
-    title: String
+    title: String,
+    onBackNavClicked: () -> Unit = {}
+) {
+    val navigationIcon : (@Composable () -> Unit)? =
+        if (!title.contains("WishList")) {
+        {
+            IconButton(onClick = { onBackNavClicked() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.arrow_back),
+                    contentDescription = "Back",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    } else {
+        null
+    }
 
-){
-    TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary
-        ),
-        title = {
-            Text(
-                text = title,
-                color = MaterialTheme.colorScheme.onPrimary,
-                modifier = Modifier
-                    .padding(start = 4.dp)
-                    .heightIn(max = 24.dp)
-            )
-        },
-    )
+
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            title = {
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .heightIn(max = 24.dp),
+                )
+            },
+            navigationIcon = {
+                navigationIcon
+            }
+        )
+
+
+
+
 }
--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -1,17 +1,45 @@
 package com.example.wishlistapp
 
-import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
+    val context = LocalContext.current
     Scaffold(
-        topBar = { AppBarView(title = " WishList ") }
+        topBar = { AppBarView(title = " WishList ") {
+            Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show()
+        }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier
+                    .padding(20.dp),
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                onClick = { Toast.makeText(context, "FAB clicked", Toast.LENGTH_LONG).show()}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
     ) { innerPadding ->
         LazyColumn(
             modifier = Modifier
