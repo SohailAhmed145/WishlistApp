4aff0990c53228abc1b990924dad3f2a2b85afe3
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-06-21 04:31:34 +0500

    files updated

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -2,20 +2,30 @@ package com.example.wishlistapp
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.wishlistapp.data.DummyWish
+import com.example.wishlistapp.data.Wish
 
 @Composable
 fun HomeView() {
@@ -46,7 +56,35 @@ fun HomeView() {
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
+            items(DummyWish.wishList){
+                wish -> WishItem(wish = wish) {
 
+            }
+            }
+        }
+    }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit){
+    Card (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .clickable {
+                onClick()
+            },
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        )
+    ){
+        Column (
+            modifier = Modifier
+                .padding(16.dp)
+        ){
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
         }
     }
 }
--- app/src/main/java/com/example/wishlistapp/MainActivity.kt
+++ app/src/main/java/com/example/wishlistapp/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wishlistapp.ui.theme.WishlistAppTheme
 
@@ -22,7 +24,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WishlistAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
                     HomeView()
                 }
             }
