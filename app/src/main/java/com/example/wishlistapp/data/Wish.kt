8ac4ae2caaf27aae9c2f2c2d21f52eec0571d1c7
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-06-21 04:31:46 +0500

    files updated

---
+++ app/src/main/java/com/example/wishlistapp/data/Wish.kt
@@ -0,0 +1,24 @@
+package com.example.wishlistapp.data
+
+
+
+
+data class Wish(
+
+    val id: Long = 0L,
+    val title: String = "",
+    val description: String = ""
+)
+
+object DummyWish{
+    val wishList = listOf(
+        Wish(1, "Title 1", "Description 1"),
+        Wish(2, "Title 2", "Description 2"),
+        Wish(3, "Title 3", "Description 3"),
+        Wish(4, "Title 4", "Description 4"),
+        Wish(5, "Title 5", "Description 5"),
+        Wish(6, "Title 6", "Description 6"),
+    )
+}
+
+
