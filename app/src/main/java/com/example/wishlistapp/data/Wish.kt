83b39e99662b31d14054fed3d7d28415acae5821
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-06-22 22:07:19 +0500

    files updated

--- app/src/main/java/com/example/wishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/wishlistapp/data/Wish.kt
@@ -9,16 +9,16 @@ data class Wish(
     val title: String = "",
     val description: String = ""
 )
-//
-//object DummyWish{
-//    val wishList = listOf(
-//        Wish(1, "Title 1", "Description 1"),
-//        Wish(2, "Title 2", "Description 2"),
-//        Wish(3, "Title 3", "Description 3"),
-//        Wish(4, "Title 4", "Description 4"),
-//        Wish(5, "Title 5", "Description 5"),
-//        Wish(6, "Title 6", "Description 6"),
-//    )
-//}
+
+object DummyWish{
+    val wishList = listOf(
+        Wish(1, "Title 1", "Description 1"),
+        Wish(2, "Title 2", "Description 2"),
+        Wish(3, "Title 3", "Description 3"),
+        Wish(4, "Title 4", "Description 4"),
+        Wish(5, "Title 5", "Description 5"),
+        Wish(6, "Title 6", "Description 6"),
+    )
+}
 
 
