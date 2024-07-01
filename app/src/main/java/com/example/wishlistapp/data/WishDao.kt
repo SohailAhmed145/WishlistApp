501416d79234c91a31121cfe1fe0a334766151ba
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-07-01 15:30:42 +0500

    files updated

--- app/src/main/java/com/example/wishlistapp/data/WishDao.kt
+++ app/src/main/java/com/example/wishlistapp/data/WishDao.kt
@@ -1,4 +1,19 @@
 package com.example.wishlistapp.data
 
-class WishDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class WishDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract fun addAWish(wishEntity: Wish)
+
+    //loads all wishes from wish table
+    @Query("SELECT * FROM 'wish-table' ")
+    abstract fun getAllWishes(): Flow<List<Wish>>
+
 }
