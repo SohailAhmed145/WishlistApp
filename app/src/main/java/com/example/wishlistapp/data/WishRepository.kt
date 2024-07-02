bfc15751375a96d5d7de2323dadb4392fba4adb2
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-07-02 15:43:27 +0500

    files updated

--- app/src/main/java/com/example/wishlistapp/data/WishRepository.kt
+++ app/src/main/java/com/example/wishlistapp/data/WishRepository.kt
@@ -1,9 +1,26 @@
 package com.example.wishlistapp.data
 
+import kotlinx.coroutines.flow.Flow
+
 class WishRepository(private val wishDao: WishDao) {
 
 
     suspend fun addWish(wish: Wish) {
         wishDao.addAWish(wish)
     }
+
+    fun getWishes(): Flow<List<Wish>> = wishDao.getAllWishes()
+
+    fun getWishById(id: Long): Flow<Wish> {
+        return wishDao.getAWish(id)
+    }
+
+    suspend fun updateWish(wish: Wish) {
+        wishDao.updateAWish(wish)
+    }
+
+    suspend fun deleteWish(wish: Wish) {
+        wishDao.deleteAWish(wish)
+    }
+
 }
